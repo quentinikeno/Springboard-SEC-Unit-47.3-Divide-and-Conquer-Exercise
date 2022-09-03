32245d92a68bde922957cdaa44b7ae3eb5a1d923
quentinikeno <ikenoq@gmail.com>
2022-09-02 15:51:39 -1000

    Refactor code to find the minIndex.

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -5,17 +5,41 @@ function findRotatedIndex(arr, target) {
 	if (arr[leftIndex] === target) return leftIndex;
 	if (arr[rightIndex] === target) return rightIndex;
 
+	const minIndex = findMinIndex(arr, leftIndex, rightIndex);
+	const maxIndex = minIndex - 1;
+
+	return arr[leftIndex] < target && target <= arr[maxIndex]
+		? findIndex(arr, target, leftIndex, maxIndex)
+		: findIndex(arr, target, minIndex, rightIndex);
+}
+
+function findIndex(arr, target, leftIndex, rightIndex) {
 	while (leftIndex <= rightIndex) {
 		let midIndex = Math.floor((leftIndex + rightIndex) / 2);
 		let midVal = arr[midIndex];
-		let leftVal = arr[leftIndex];
+
+		if (arr[leftIndex] === target) return leftIndex;
+		if (arr[rightIndex] === target) return rightIndex;
 
 		if (midVal === target) {
 			return midIndex;
-		} else if (
-			(midVal > target && leftVal > target) ||
-			(midVal < target && leftVal < target)
-		) {
+		} else if (midVal < target) {
+			leftIndex = midIndex + 1;
+		} else {
+			rightIndex = midIndex - 1;
+		}
+	}
+	return -1;
+}
+
+function findMinIndex(arr, leftIndex, rightIndex) {
+	while (leftIndex <= rightIndex) {
+		let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+		let midVal = arr[midIndex];
+
+		if (midVal < arr[midIndex - 1]) {
+			return midIndex;
+		} else if (midVal > arr[rightIndex]) {
 			leftIndex = midIndex + 1;
 		} else {
 			rightIndex = midIndex - 1;
