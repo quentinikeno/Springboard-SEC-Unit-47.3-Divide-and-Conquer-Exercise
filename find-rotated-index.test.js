07029e3743c0c728366fa5c58a8346fbb73269d9
quentinikeno <ikenoq@gmail.com>
2022-09-02 14:37:35 -1000

    Add more tests.

--- find-rotated-index.test.js
+++ find-rotated-index.test.js
@@ -6,4 +6,13 @@ it("should find the index of an integer in a rotated array", () => {
 	expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)).toBe(6);
 	expect(findRotatedIndex([37, 44, 66, 102, 10, 22], 14)).toBe(-1);
 	expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12)).toBe(-1);
+	expect(findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)).toBe(6);
+	expect(findRotatedIndex([37, 44, 66, 102, 10, 22], 102)).toBe(3);
+	expect(findRotatedIndex([37, 44, 66, 102, 10, 22], 44)).toBe(1);
+	expect(
+		findRotatedIndex([37, 44, 66, 102, 10, 22, 23, 25, 26, 27, 30, 31], 44)
+	).toBe(1);
+	expect(
+		findRotatedIndex([37, 44, 66, 102, 10, 22, 23, 25, 26, 27, 30, 31], 30)
+	).toBe(10);
 });
